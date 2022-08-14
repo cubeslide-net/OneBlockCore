cc1a1c3955e6e45a76cc2fad393cd55acf5ba326
Luca D. (SchweizerDev) <luca.dubi@bitvoodoo.ch>
2022-08-14 13:32:13 +0200

    added new variable and new method for auctionhouse

--- src/main/java/net/cubeslide/oneblock/oneblockcore/auctionhouse/AuctionManager.java
+++ src/main/java/net/cubeslide/oneblock/oneblockcore/auctionhouse/AuctionManager.java
@@ -1,9 +1,21 @@
 package net.cubeslide.oneblock.oneblockcore.auctionhouse;
 
-import java.util.Random;
+import net.cubeslide.oneblock.oneblockcore.utils.ItemBuilder;
+import net.cubeslide.oneblock.oneblockcore.utils.Pagifier;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.*;
 
 public class AuctionManager {
 
+    public static List<AuctionItem> auctionItems = new ArrayList<AuctionItem>();
+    public static List<AuctionItem> expiredAuctionItems = new ArrayList<AuctionItem>();
+
+    private static ItemStack offers = new ItemBuilder(Material.EMERALD).name("§eOffers").lore(Arrays.asList("§7Here you can find all offers", "§7from other players")).build();
+    private static ItemStack myOffers = new ItemBuilder(Material.CHEST_MINECART).name("§eMy offers").lore(Arrays.asList("§7Here you find your auction items in the auctionhouse")).build();
+    private static ItemStack offerHelp = new ItemBuilder(Material.REDSTONE).name("§cNo Items found!").lore(Arrays.asList("§7You didn't offer any items in the moment.", "§7Click on a item in your inventory,", "§7to sell it in the auctionhouse")).build();
 
     public static String getRandomKey() {
         String pattern = "";
@@ -17,5 +29,93 @@ public class AuctionManager {
             builder.append(pattern.charAt(random.nextInt(pattern.length())));
         }
         return builder.toString();
+    }
+
+    public static String getTime(long expires) {
+        long d = expires / 1000 - System.currentTimeMillis() / 1000;
+        int seconds = (int) d;
+        String s = "Unknown";
+        int hours = seconds / 3600;
+        int minutes = seconds / 60;
+        if(System.currentTimeMillis() > expires) {
+            return "Expired";
+        }
+        if(hours != 0) {
+            if(hours == 1) {
+                s = hours + " hour";
+            } else {
+                s = hours + " hours";
+            }
+        } else if(minutes != 0) {
+            if(minutes == 1) {
+                s = minutes + " minute";
+            } else {
+                s = minutes + " minutes";
+            }
+        } else {
+            if(seconds == 1) {
+                s = seconds + " second";
+            } else {
+                s = seconds + " seconds";
+            }
+        }
+        return s;
+    }
+
+    public static void addAuctionItem(Player player, ItemStack itemStack, int price, long expires) {
+        auctionItems.add(new AuctionItem(player.getUniqueId(), player.getName(), itemStack, price, expires));
+    }
+
+    public static void removeAuctionItem(Player giveTo, AuctionItem item) {
+        if (auctionItems.contains(item)) {
+            auctionItems.remove(item);
+            giveTo.getInventory().addItem(item.getItem());
+        }
+    }
+
+    public static List<AuctionItem> getAuctionItemsForUUID(UUID uuid) {
+        List<AuctionItem> items = new ArrayList<>();
+        for (AuctionItem ai : auctionItems) {
+            if (ai.getOwner().toString().equalsIgnoreCase(uuid.toString())) {
+                items.add(ai);
+            }
+        }
+        for (AuctionItem ai : expiredAuctionItems) {
+            if (ai.getOwner().toString().equalsIgnoreCase(uuid.toString())) {
+                items.add(ai);
+            }
+        }
+        return items;
+    }
+
+    public static AuctionItem getItemFromId(String id, boolean expired) {
+        for (AuctionItem ai : auctionItems) {
+            if(ai.getItemID().equalsIgnoreCase(id)) {
+                return ai;
+            }
+        }
+        if(expired) {
+            for(AuctionItem ai : expiredAuctionItems) {
+                if(ai.getItemID().equalsIgnoreCase(id)) {
+                    return ai;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static Pagifier<AuctionItem> getAuctionItems() {
+        Pagifier<AuctionItem> pages = new Pagifier<AuctionItem>(4*9);
+        Iterator<AuctionItem> items = auctionItems.iterator();
+        while (items.hasNext()) {
+            AuctionItem item = items.next();
+            if (item.isExpired()) {
+                expiredAuctionItems.add(item);
+                items.remove();
+                continue;
+            }
+            pages.addItem(item);
+        }
+        return pages;
     }
 }
